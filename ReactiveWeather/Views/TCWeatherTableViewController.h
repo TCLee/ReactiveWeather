7707ebb0b7363c85a245bdf83436b6e634bf8de0
Lee Tze Cheun <tzecheun.lee@gmail.com>
2014-04-26 11:11:26 +0800

    Connected table view controller to custom class.

---
+++ ReactiveWeather/Views/TCWeatherTableViewController.h
@@ -0,0 +1,11 @@
+//
+//  TCWeatherTableViewController.h
+//  ReactiveWeather
+//
+//  Created by Lee Tze Cheun on 4/26/14.
+//  Copyright (c) 2014 Lee Tze Cheun. All rights reserved.
+//
+
+@interface TCWeatherTableViewController : UITableViewController
+
+@end
