8b6d38196b2bd453fed529408dc9c3a113e66bd5
Lee Tze Cheun <tzecheun.lee@gmail.com>
2014-04-27 11:42:50 +0800

    Comment fix. Made it clearer.

--- ReactiveWeather/TCAppDelegate.m
+++ ReactiveWeather/TCAppDelegate.m
@@ -17,12 +17,16 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-    // Create the view model with its service classes.
+    // We do not need very high accuracy for the location services, since
+    // we're only using the location to fetch weather data for a city.
     TCLocationService *locationService =
         [[TCLocationService alloc] initWithAccuracy:kCLLocationAccuracyKilometer
                                      distanceFilter:1000.0f
                                         maxCacheAge:15.0f];
+
     TCWeatherService *weatherService = [[TCWeatherService alloc] init];
+
+    // Create the view model with the given service classes.
     TCWeatherViewModel *viewModel =
         [[TCWeatherViewModel alloc] initWithLocationService:locationService
                                              weatherService:weatherService];
