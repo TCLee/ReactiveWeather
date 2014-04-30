aa92b1abafdcb807e9d86d2627b80bee43a853eb
Lee Tze Cheun <tzecheun.lee@gmail.com>
2014-04-30 12:31:35 +0800

    Added blur effect using reactive bindings.

--- ReactiveWeather/Views/TCWeatherViewController.m
+++ ReactiveWeather/Views/TCWeatherViewController.m
@@ -10,14 +10,14 @@
 #import "TCWeatherTableViewController.h"
 #import "TCWeatherViewModel.h"
 
-#import <LBBlurredImage/UIImageView+LBBlurredImage.h>
+#import <FXBlurView/FXBlurView.h>
 
 static NSString * const TCChildSegueIdentifier = @"TCChildTableViewController";
 
 @interface TCWeatherViewController ()
 
 @property (nonatomic, weak) IBOutlet UIImageView *backgroundImageView;
-@property (nonatomic, weak) IBOutlet UIImageView *blurredImageView;
+@property (nonatomic, weak) IBOutlet FXBlurView *blurFXView;
 
 @end
 
@@ -34,9 +34,40 @@ static NSString * const TCChildSegueIdentifier = @"TCChildTableViewController";
 
     // Add blur effect to background image. This will not take effect
     // until the user scrolls.
-    [self.blurredImageView setImageToBlur:self.backgroundImageView.image
-                               blurRadius:10.0f
-                          completionBlock:nil];
+    self.blurFXView.underlyingView = self.backgroundImageView;
+    self.blurFXView.dynamic = NO;
+    self.blurFXView.blurRadius = 20;
+    self.blurFXView.tintColor = nil;
+    self.blurFXView.blurEnabled = NO;
+    self.blurFXView.alpha = 0;
+
+    TCWeatherTableViewController *childTableViewController = self.childViewControllers.firstObject;
+    const CGFloat tableHeight = childTableViewController.tableView.bounds.size.height;
+
+    // When user scrolls down, the background image will be blurred.
+    // Alternatively, when user scrolls back up the background image will
+    // be clear again.
+
+    RAC(self.blurFXView, blurEnabled) = [[[RACObserve(self.blurFXView, alpha)
+        distinctUntilChanged]
+        map:^(NSNumber *alpha) {
+            NSLog(@"Alpha = %.2f", alpha.floatValue);
+            return (alpha.floatValue > 0 ? @YES : @NO);
+        }]
+        startWith:@NO];
+
+    RAC(self.blurFXView, alpha) = [[[RACObserve(childTableViewController, tableView.contentOffset)
+        distinctUntilChanged]
+        map:^(NSValue *contentOffset) {
+            NSLog(@"Scroll Position = %.2f", contentOffset.CGPointValue.y);
+            // Scrolling past the top of the table must not affect the
+            // blur alpha.
+            CGFloat scrollPosition = MAX(0, contentOffset.CGPointValue.y);
+
+            // Alpha should not exceed 1.0
+            return @(MIN(1, scrollPosition / tableHeight));
+        }]
+        startWith:@0];
 }
 
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
