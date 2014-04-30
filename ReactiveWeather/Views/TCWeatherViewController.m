f2df638727b2ca5d81ee641e1aeecc904881da76
Lee Tze Cheun <tzecheun.lee@gmail.com>
2014-04-30 16:53:10 +0800

    Refactored code to make it look less ugly.

--- ReactiveWeather/Views/TCWeatherViewController.m
+++ ReactiveWeather/Views/TCWeatherViewController.m
@@ -12,8 +12,6 @@
 
 #import <FXBlurView/FXBlurView.h>
 
-static NSString * const TCChildSegueIdentifier = @"TCChildTableViewController";
-
 @interface TCWeatherViewController ()
 
 @property (nonatomic, weak) IBOutlet UIImageView *backgroundImageView;
@@ -32,34 +30,44 @@ static NSString * const TCChildSegueIdentifier = @"TCChildTableViewController";
 {
     [super viewDidLoad];
 
-    // Add blur effect to background image. This will not take effect
-    // until the user scrolls.
     self.blurFXView.underlyingView = self.backgroundImageView;
-    self.blurFXView.dynamic = NO;
-    self.blurFXView.blurRadius = 20;
-    self.blurFXView.tintColor = nil;
-    self.blurFXView.blurEnabled = NO;
-    self.blurFXView.alpha = 0;
+    RAC(self.blurFXView, blurEnabled) = [self shouldEnableBlur];
+    RAC(self.blurFXView, alpha) = [self verticalContentOffsetToAlpha];
+}
 
-    TCWeatherTableViewController *childTableViewController = self.childViewControllers.firstObject;
-    const CGFloat tableHeight = childTableViewController.tableView.bounds.size.height;
+#pragma mark Blur View Effect
 
-    // When user scrolls down, the background image will be blurred.
-    // Alternatively, when user scrolls back up the background image will
-    // be clear again.
-
-    RAC(self.blurFXView, blurEnabled) = [[[RACObserve(self.blurFXView, alpha)
+/**
+ * Returns a signal of @c NSNumber BOOL values indicating if the blur
+ * effect should be enabled or not.
+ */
+- (RACSignal *)shouldEnableBlur
+{
+    return [[[RACObserve(self.blurFXView, alpha)
         distinctUntilChanged]
         map:^(NSNumber *alpha) {
-            NSLog(@"Alpha = %.2f", alpha.floatValue);
+            // Only enable blur effects if blur view's alpha has changed.
             return (alpha.floatValue > 0 ? @YES : @NO);
         }]
         startWith:@NO];
+}
 
-    RAC(self.blurFXView, alpha) = [[[RACObserve(childTableViewController, tableView.contentOffset)
+/**
+ * Returns a signal of @c NSNumber values from @c 0.0 to @c 1.0
+ * representing the alpha of a UIView.
+ *
+ * The alpha value is inversely proportional to the table view's content
+ * offset's Y position. E.g. Scroll down to get alpha values towards
+ * 1.0; scroll up to get alpha values towards 0.0.
+ */
+- (RACSignal *)verticalContentOffsetToAlpha
+{
+    TCWeatherTableViewController *childTableViewController = self.childViewControllers.firstObject;
+    const CGFloat tableHeight = childTableViewController.tableView.bounds.size.height;
+
+    return [[[RACObserve(childTableViewController, tableView.contentOffset)
         distinctUntilChanged]
         map:^(NSValue *contentOffset) {
-            NSLog(@"Scroll Position = %.2f", contentOffset.CGPointValue.y);
             // Scrolling past the top of the table must not affect the
             // blur alpha.
             CGFloat scrollPosition = MAX(0, contentOffset.CGPointValue.y);
@@ -70,12 +78,15 @@ static NSString * const TCChildSegueIdentifier = @"TCChildTableViewController";
         startWith:@0];
 }
 
+#pragma mark Storyboard Segue
+
+static NSString * const TCChildSegueIdentifier = @"TCChildTableViewController";
+
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
 {
     if ([segue.identifier isEqualToString:TCChildSegueIdentifier]) {
         // Pass the view model to the child table view controller.
-        TCWeatherTableViewController *childTableViewController =
-            segue.destinationViewController;
+        TCWeatherTableViewController *childTableViewController = segue.destinationViewController;
         childTableViewController.viewModel = self.viewModel;
     }
 }
